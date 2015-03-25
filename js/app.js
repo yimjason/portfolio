09f24768eed08745f17061b8ac3d30788fbc8c17
Jason <yimjason00@gmail.com>
2015-03-24 21:42:09 -0400

    customized scrolling

--- js/app.js
+++ js/app.js
@@ -3,10 +3,29 @@ $( document ).ready(function() {
 	//**************SMOOTH SCROLL NAVIGATION************
 	$(".nav_link").click(function(event){
 		event.preventDefault();
-		var dest=$(this.hash).offset().top-38; 
-		$('html,body').animate({scrollTop:dest}, 1000,'swing');
+		var current_position = window.pageYOffset;
+		console.log(current_position);
+		var dest = $(this.hash).offset().top-38;
+		console.log("dest: "+dest); 
+		var distance = Math.abs(dest - current_position);
+		var speed = 2.5;
+		var time = distance/speed;
+		if(current_position<dest){
+			var sling	= -(distance/20);
+			var overshoot = distance/20;
+		}else{
+			sling = distance/20;
+			overshoot = -distance/20;
+		}
+		if(current_position != 0){
+			console.log('hey');
+			$('html,body').animate({scrollTop:current_position+sling}, time/2.3,'swing');
+		}
+		if(dest != -38){
+			$('html,body').animate({scrollTop:dest+overshoot}, time,'swing');
+		}
+		$('html,body').animate({scrollTop:dest}, time/2,'swing');
 	});
-
 
 	//*************SET UP CAROUSEL ANIMATION FOR EACH GALLERY IN PORTFOLIO****************************
 
