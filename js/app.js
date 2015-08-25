a7032d27a42110a517c6399ef69749f2d9f1e840
Jason <yimjason00@gmail.com>
2015-08-24 22:28:48 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,14 +1,5 @@
 $( document ).ready(function() {
-  // var current_position = window.pageYOffset; 
-  // console.log($("#artwork").offset().top);
-		// var artwork = $("#artwork").offset().top-38;
-		// console.log("curr:" + current_position);
-		// if(current_position > artwork){
-		// 	console.log("hey");
-		// 	$("nav").children().first().children().first().next().children().first().css("background-color", "whitesmoke");
-		// }
-
-	//**************SMOOTH SCROLL NAVIGATION************
+ 
 	$(".nav_link").click(function(event){
 		event.preventDefault();
 		var current_position = window.pageYOffset; 
@@ -32,22 +23,6 @@ $( document ).ready(function() {
 		}
 		$('html,body').animate({scrollTop:dest}, time/2,'swing');
 	});
-
-	//*************SET UP CAROUSEL ANIMATION FOR EACH GALLERY IN PORTFOLIO****************************
-
-	// var galleryNums = ["1","2","3","4","5","6"];
-    
-	// for (i = 0; i < galleryNums.length; i++) { 
-
-	// 	$("#owl-demo" + galleryNums[i]).owlCarousel({
-	//       navigation : true, // Show next and prev buttons
-	//       slideSpeed : 300,
-	//       paginationSpeed : 400,
-	//       singleItem:true,
-	//       pagination : false
-	// 	});
-	// };
-
-	
+ 	
 });
 
